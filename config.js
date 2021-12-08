6641aaf4cb0fbfb64fbb9b7aa15993b33675c790
ngthuy-2k7 <95755436+ngthuy-2k7@users.noreply.github.com>
2021-12-08 22:06:48 +0700

    Update config.js

--- config.js
+++ config.js
@@ -1,13 +1,20 @@
 const CONFIG = {
     titleWeb: "Lời xin lỗi",
-    title: 'Tụi em xin lỗi vì chiều nay đã làm cô buồn',
-    desc: 'Cô có thể tha lỗi cho tụi em được hông ạ ^^',
-    btnYes: 'Ừ, cô sẽ tha lỗi cho mấy đứa',
+    introTitle: 'Cô Lam dạy hóa',
+    introDesc: `Trái đất vốn lạ thường
+    Mà sao cô cứ đi nhầm đường
+    Lạc vào tim em lẻ loi
+    Đằng sau chữ yêu đây là thương`,
+    btnIntro: '^^HiHi^^',
+    title: 'Tụi em xin lỗi vì chiều nay đã làm cho cô không được vui',
+    desc: 'Cô tha lỗi cho tụi em được không ạ ',
+    btnYes: 'Ừ cô sẽ tha cho mấy đứa',
     btnNo: 'Không nha :3',
-    question: 'Tại sao cô lại tha lỗi cho tụi em vậy',
-    btnReply: 'Gửi cho các em',
-    reply: 'Tại cô yêu tụi em nhiều lắm luôn ó :33',
-    mess: 'Hihi, tụi em biết mà, tụi em cũng yêu cô nhiềuuuuu',
-    messDesc: 'Chủ nhật tuần này mình lại học tiếp nha cô, tụi em hứa sẽ tương tác đầy đủ và không vào muộn nữa',
+    question: 'Tại sao cô lại tha lỗi cho tụi em vậy ạ',
+    btnReply: 'Gửi cho bạn <3',
+    reply: 'Tại vì cô yêu tụi em nhiều lắm ó <333',
+    mess: 'Tụi em biết mà 🥰. Tụi em cũng yêu cô nhiều nhiều 😘😘',
+    messDesc: 'Chiều chủ nhật tuần này mình lại học tiếp nha cô, tụi em hứa sẽ tương tác với cô nhiều hơn và không vào muộn nữa. Cô xem xong nhớ qua group zalo nhắn cho tụi em biết nhaaa',
     btnAccept: 'Okiiiii lun <3',
-   
+    messLink: 'https://github.com/zukahai/Confess-Crush' //link mess của các bạn. VD: https://m.me/nam.nodemy
+}
