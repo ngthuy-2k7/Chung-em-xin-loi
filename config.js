d4a03f2b80af61538a74ace3c3952d0f4ec8127f
ngthuy-2k7 <95755436+ngthuy-2k7@users.noreply.github.com>
2021-12-09 10:33:37 +0700

    Update config.js

--- config.js
+++ config.js
@@ -1,17 +1,20 @@
 const CONFIG = {
     titleWeb: "Lời xin lỗi",
-    introTitle: 'Cô Lam dạy hóa',
-    introDesc: `Trái đất vốn lạ thường
-                Mà sao cô cứ đi nhầm đường
-                Lạc vào tim em lẻ loi
-                Đằng sau chữ yêu đây là thương`,
+    introTitle: 'Gửi cô Lam dạy hóa',
+    introDesc: ` Trái đất vốn lạ thường
+               
+                 Mà sao cô cứ đi nhầm đường
+                 
+                 Lạc vào tim em lẻ loi
+                 
+                 Đằng sau chữ yêu đây là thương`,
     btnIntro: '^^HiHi^^',
     title: 'Tụi em xin lỗi vì chiều nay đã làm cho cô không được vui',
     desc: 'Cô tha lỗi cho tụi em được không ạ ',
     btnYes: 'Ừ cô sẽ tha cho mấy đứa',
     btnNo: 'Không nha :3',
     question: 'Tại sao cô lại tha lỗi cho tụi em vậy ạ',
-    btnReply: 'Gửi cho bạn <3',
+    btnReply: 'Gửi cho các em <3',
     reply: 'Tại vì cô yêu tụi em nhiều lắm ó <333',
     mess: 'Tụi em biết mà 🥰. Tụi em cũng yêu cô nhiều nhiều 😘😘',
     messDesc: 'Chiều chủ nhật tuần này mình lại học tiếp nha cô, tụi em hứa sẽ tương tác với cô nhiều hơn và không vào muộn nữa. Cô xem xong nhớ quét mã QR nha cô',
